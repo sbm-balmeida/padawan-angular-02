6790e5c91cd4cfc167c4f4c947c0fbb3faa44624
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-05 15:22:27 -0300

    Organizando as rotas

--- gatitobook/src/app/app.module.ts
+++ gatitobook/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { HomeModule } from './home/home.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HomeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
