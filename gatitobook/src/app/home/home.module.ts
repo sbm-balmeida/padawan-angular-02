8cdcbc25460b98e0d40abf0ba7baad7e8e5f8213
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-06 10:07:52 -0300

    Formulário Template-Driven

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
   ],
   exports:[HomeComponent],
 })
