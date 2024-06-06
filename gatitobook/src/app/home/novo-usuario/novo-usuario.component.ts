97a8ccd195e2958764db65e8f935bf6b3b55cedd
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-06 14:40:05 -0300

    Validação de Formulário

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { minusculoValidator } from './minusculo.validator';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -26,7 +27,11 @@ export class NovoUsuarioComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator], [this.usuarioExisteService.usuarioJaExiste()]],
       password: [''],
-    });
+    },
+    {
+      validators: [usuarioSenhaIguaisValidator],
+    }
+  );
   }
 
   cadastrar() {
